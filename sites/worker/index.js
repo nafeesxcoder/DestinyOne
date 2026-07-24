453921c037aa68b48f38c12d77788239d496c897
Shivay <shivay@Shivays-MacBook-Pro.local>
2026-07-23 17:19:12 -0700

    Serve web entrypoint from site root

--- sites/worker/index.js
+++ sites/worker/index.js
@@ -1,5 +1,10 @@
 export default {
   fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === '/') {
+      url.pathname = '/index.html';
+      return env.ASSETS.fetch(new Request(url, request));
+    }
     return env.ASSETS.fetch(request);
   },
 };
